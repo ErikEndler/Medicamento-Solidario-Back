374fe5b2c82279c8237ec5197911c9af3d87b19a
Erik Endler <eng.erik.endler@gmail.com>
2020-03-16 18:42:27 -0300

    cors()2.0
    tentando desabilitar

--- src/main/java/com/apirest/MedicamentoSolidario/security/ResourceServerConfig.java
+++ src/main/java/com/apirest/MedicamentoSolidario/security/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.apirest.MedicamentoSolidario.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -13,12 +14,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
     	http.csrf().disable();
-    	http.cors();
+    	http.cors().disable();
         http.authorizeRequests()
         		.antMatchers(publicEndPoints()).permitAll() 
                 .antMatchers(HttpMethod.POST, "/api/usuario").permitAll()
                 .antMatchers(HttpMethod.GET,  "/api/usuario").permitAll()                               
                 .anyRequest().authenticated();
+        http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
     }
     private String[] publicEndPoints() {
         return new String[] {  "/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**",
--- src/main/java/com/apirest/MedicamentoSolidario/security/SimpleCORSFilter.java
+++ src/main/java/com/apirest/MedicamentoSolidario/security/SimpleCORSFilter.java
@@ -10,26 +10,49 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@Component
-public class SimpleCORSFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.setHeader("Access-Control-Expose-Headers", "Location");
-        chain.doFilter(req, res);
+//@Component
+//public class SimpleCORSFilter implements Filter {
+//
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse response = (HttpServletResponse) res;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//        response.setHeader("Access-Control-Expose-Headers", "Location");
+//        chain.doFilter(req, res);
+//    }
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) {}
+//
+//    @Override
+//    public void destroy() {}
+//
+//}
+@Configuration
+public class SimpleCORSFilter {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
-
-    @Override
-    public void init(FilterConfig filterConfig) {}
-
-    @Override
-    public void destroy() {}
-
 }
