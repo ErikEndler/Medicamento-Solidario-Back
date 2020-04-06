09e6f6054b5bf653ee7e87da54e2b3e4d291a8bc
Erik Endler <eng.erik.endler@gmail.com>
2020-04-06 20:35:36 -0300

    ajuste endpoints

--- src/main/java/com/apirest/MedicamentoSolidario/resources/UsuarioResources.java
+++ src/main/java/com/apirest/MedicamentoSolidario/resources/UsuarioResources.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping(value = "/api/usuario")
 @Api(value = "API REST Usuario")
-//@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioResources {
 
 	@Autowired
