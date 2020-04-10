6cb3ff0ad57e4a3f14eabc2823494934a83ff5c4
Erik Endler <eng.erik.endler@gmail.com>
2020-04-10 14:52:04 -0300

    Update SimpleCORSFilter.java

--- src/main/java/com/apirest/MedicamentoSolidario/security/SimpleCORSFilter.java
+++ src/main/java/com/apirest/MedicamentoSolidario/security/SimpleCORSFilter.java
@@ -22,7 +22,7 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
        // response.setHeader("Access-Control-Max-Age", "33600");
         response.setHeader("Access-Control-Allow-Headers", "*");
